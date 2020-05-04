7089171e9d930c101112d63cf850bddcc783d36a
allen <theallenma@gmail.com>
2020-05-03 19:17:13 -0700

    ShoppingList: A React Native Playground

---
+++ App.js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import { View, Text, FlatList, StyleSheet, Alert } from 'react-native';
+import Header from './components/Header';
+import ListItem from './components/ListItem';
+import AddItem from './components/AddItem';
+
+import { v4 as uuidv4 } from 'uuid';
+
+const App = () => {
+  const [items, setItems] = useState([
+    {
+      id: uuidv4(),
+      text: 'Milk',
+    },
+    {
+      id: uuidv4(),
+      text: 'Eggs',
+    },
+    {
+      id: uuidv4(),
+      text: 'Bread',
+    },
+    {
+      id: uuidv4(),
+      text: 'Juice',
+    },
+  ]);
+
+  const deleteItem = (id) => {
+    setItems((prevItems) => {
+      return prevItems.filter((item) => item.id !== id);
+    });
+  };
+
+  const addItem = (text) => {
+    if (!text) {
+      Alert.alert('Error', 'Please enter an item', { text: 'OK' });
+    } else {
+      setItems((prevItems) => {
+        return [...prevItems, { id: uuidv4(), text }];
+      });
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Header title="Shopping List" />
+      <AddItem addItem={addItem} />
+      <FlatList
+        data={items}
+        renderItem={({ item }) => (
+          <ListItem item={item} deleteItem={deleteItem} />
+        )}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 60,
+  },
+});
+
+export default App;
